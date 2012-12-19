26f6704db968971b84200b83a4c4bb2266556a62
Harry Jeffery <harry@exec64.co.uk>
2012-12-19 14:21:11 +0000

    Changed multi monitor configuration

--- .i3/focused_output.py
+++
@@ -1,5 +0,0 @@
-import json, sys
-
-for l in json.load(sys.stdin):
-	if l['focused'] == True:
-		print l['output']
--- .i3/move_workspace_to_current_output.sh
+++
@@ -1,6 +0,0 @@
-output=$(i3-msg -t get_workspaces | python ~/.i3/focused_output.py)
-
-i3-msg workspace $1
-i3-msg move workspace to output $output
-sleep 0.1
-i3-msg workspace $1
