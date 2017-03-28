f2cdb8c1195b67d810033634fe7c92479517d1d1
Harry Jeffery <harry@exec64.co.uk>
2017-03-28 21:13:29 +0100

    Update environment

--- dotfiles/_i3_start.sh
+++ dotfiles/_i3_start.sh
@@ -10,3 +10,9 @@ xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
 xmodmap -e 'clear mod4' -e 'clear control' -e 'add control = Control_L' -e 'add mod4 = Control_R'
 
 xrdb ~/.Xresources
+xrandr --dpi 100
+
+#nosleep
+xset -dpms
+xset s noblank
+xset s off
